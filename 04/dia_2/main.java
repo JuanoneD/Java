6cefe0caa6041f769e89a78b8db8358f996efc40
Juan Diego <134559247+JuanoneD@users.noreply.github.com>
2024-05-02 11:42:51 -0300

    Create main.java

---
+++ 04/dia_2/main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class main {
+    void printMenu(){
+        System.out.println("Escreva o ");
+    }
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        String CPF,password;
+
+        Employer admin = new Employer("Admin", "Admin", "Admin", "Admin", true);
+        ListNode list = new ListNode();
+
+        list.addEmployer(admin);
+
+        while(true){
+
+            System.out.println("Escreva o CPF");
+            CPF = scan.nextLine();
+            System.out.println("Escreva sua senha");
+            password = scan.nextLine();
+
+            
+
+
+        }
+
+    }
+}
