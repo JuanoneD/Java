cc82a674d46013e5c236e2e52ca5f90bd0402ba7
Juan Diego <134559247+JuanoneD@users.noreply.github.com>
2024-05-16 11:52:23 -0300

    Add files via upload

---
+++ 05/dia_16/greenhill/collections/Arraylist.java
@@ -0,0 +1,63 @@
+package greenhill.collections;
+
+public class Arraylist<T>  extends List{
+
+    private T [] array;
+
+    Arraylist(int size){
+
+        array = ( T[] ) ( new Object[size] );
+
+    }
+
+    Arraylist(){
+
+        array = ( T[] ) ( new Object[10] );
+
+    }
+
+
+    @Override
+    public void add(Object value) {
+        if(getSize() == array.length ){
+
+            T [] copy = ( T[] ) ( new Object[ array.length * 2] );
+
+            for( int i = 0; i < array.length ; i ++){
+
+                copy[i] = array[i];
+
+            }
+
+            array = copy;
+        }
+
+        array[getSize()] = ( T ) (value);
+
+        setSize(getSize() + 1);
+
+    }
+
+
+    @Override
+    public T get(int index) {
+
+        if (index < 0 || index >= getSize())
+            throw new ArrayIndexOutOfBoundsException();
+
+        return array[index];
+
+    }
+
+    @Override
+    public void set(int index, Object value) {
+        
+        if (index < 0 || index >= getSize())
+            throw new ArrayIndexOutOfBoundsException(); 
+
+        array[index] = ( T ) (value);
+
+    }
+
+
+}
---
+++ 05/dia_16/greenhill/collections/List.java
@@ -0,0 +1,12 @@
+package greenhill.collections;
+
+abstract public class List<T> extends Sonic{
+    
+    abstract public T get(int index);
+
+    abstract public void set(int index, T value);
+
+    abstract public void add(T value);
+
+
+}
